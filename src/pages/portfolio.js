c1231bceeaf049b72efaea1cc48819c1de31ff49
Asjas <asjas@outlook.com>
2018-06-18 20:04:09 +0200

    run eslint on files

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 import { Container } from '../ui/pages/portfolio';
 import Project from '../components/project';
