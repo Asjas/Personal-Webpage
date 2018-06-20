455b887e90d80685d213a40a683a88e0cb1044ff
Asjas <asjas@outlook.com>
2018-06-20 13:09:43 +0200

    add logrocket error logging

---
+++ src/utils/logrocket.js
@@ -0,0 +1,7 @@
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+LogRocket.init('tmyqjr/personal-website-jkld5');
+
+// after calling LogRocket.init()
+export default setupLogRocketReact(LogRocket);
