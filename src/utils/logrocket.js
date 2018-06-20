3ac2256a7189dc8cee75cf5ca85ca1c9621b7eb9
Asjas <asjas@outlook.com>
2018-06-20 14:20:29 +0200

    setup rollbar and remove .babelrc file

--- src/utils/logrocket.js
+++ src/utils/logrocket.js
@@ -1,8 +1,16 @@
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
+import Rollbar from 'rollbar';
 
 export default function setupLogRocket() {
   LogRocket.init('tmyqjr/personal-website-jkld5');
   // after calling LogRocket.init()
   setupLogRocketReact(LogRocket);
+
+  Rollbar.configure({
+    transform(obj) {
+      obj.sessionURL = LogRocket.sessionURL;
+      return obj;
+    },
+  });
 }
