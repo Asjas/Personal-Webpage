29c69b707c8f7f78699cbc189679c1721a1a1479
Asjas <asjas@outlook.com>
2018-06-20 14:23:40 +0200

    remove rollbar for now

--- src/utils/logrocket.js
+++ src/utils/logrocket.js
@@ -1,16 +1,8 @@
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
-import Rollbar from 'rollbar';
 
 export default function setupLogRocket() {
   LogRocket.init('tmyqjr/personal-website-jkld5');
   // after calling LogRocket.init()
   setupLogRocketReact(LogRocket);
-
-  Rollbar.configure({
-    transform(obj) {
-      obj.sessionURL = LogRocket.sessionURL;
-      return obj;
-    },
-  });
 }
