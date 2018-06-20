30291ce68eeed1ce7c9122273cb647f838c67f26
Asjas <asjas@outlook.com>
2018-06-20 13:48:47 +0200

    fix logrocket setup and babel logrocket config

--- src/utils/logrocket.js
+++ src/utils/logrocket.js
@@ -1,7 +1,8 @@
 import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
 
-LogRocket.init('tmyqjr/personal-website-jkld5');
-
-// after calling LogRocket.init()
-export default setupLogRocketReact(LogRocket);
+export default function setupLogRocket() {
+  LogRocket.init('tmyqjr/personal-website-jkld5');
+  // after calling LogRocket.init()
+  setupLogRocketReact(LogRocket);
+}
