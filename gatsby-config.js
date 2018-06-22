355e923d266f6d8b7631ad471c491975f3e11e2a
Asjas <asjas@outlook.com>
2018-06-22 21:00:26 +0200

    remove webpack bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,13 +51,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
-      options: {
-        analyzerPort: 3000,
-        production: false,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-netlify',
       options: {
         headers: {},
