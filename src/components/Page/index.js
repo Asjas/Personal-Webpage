c0e1e0ccd7ff02b050e8f4a6f8de6f11165bbfa2
Asjas <asjas@outlook.com>
2018-11-22 18:11:49 +0200

    added nprogress bar to the website
    also changed the color of the bar from red to gold

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
+import Nprogress from 'nprogress';
 import { Section, Aside, Main } from './styles';
 import Meta from '../Meta';
 import Navbar from '../Sidebar';
+
+Router.onRouteChangeStart = () => {
+  Nprogress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  Nprogress.done();
+};
+
+Router.onRouteChangeError = () => {
+  Nprogress.done();
+};
 
 class Layout extends Component {
   static propTypes = {
