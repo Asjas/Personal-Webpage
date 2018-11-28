dd001d123f6a7ed0b62a3a0f017972827ac03bf6
Asjas <asjas@outlook.com>
2018-11-28 20:01:55 +0200

    remove grapqhl query from layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,44 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
-import Nprogress from 'nprogress';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyles } from '../../theme';
 import { Section, Aside, Main } from './styles';
 import Navbar from '../Sidebar';
 
 const Layout = ({ children }) => (
-  /* eslint no-undef: "off" */
-  <StaticQuery
-    query={graphql`
-      query LayoutQuery {
-        site {
-          siteMetadata {
-            title
-            siteUrl
-            googleSiteVerification
-            description
-            keywords
-          }
-        }
-      }
-    `}
-    render={data => (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyles />
-          {/* <Meta /> */}
-          <Section>
-            <Aside>
-              <Navbar />
-            </Aside>
-            <Main>{children}</Main>
-          </Section>
-        </>
-      </ThemeProvider>
-    )}
-  />
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyles />
+      {/* <Meta /> */}
+      <Section>
+        <Aside>
+          <Navbar />
+        </Aside>
+        <Main>{children}</Main>
+      </Section>
+    </>
+  </ThemeProvider>
 );
 
 Layout.propTypes = {
