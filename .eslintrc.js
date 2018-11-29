8991c99d4e94118599f2ea93994ba2943c7349b3
Asjas <asjas@outlook.com>
2018-11-29 08:49:30 +0200

    add gatsby as a imported module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,53 @@
 module.exports = {
-  "extends": [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "prettier/react"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "impliedStrict": true,
-      "classes": true
+  extends: ["airbnb", "plugin:prettier/recommended", "prettier/react"],
+  settings: {
+    "import/core-modules": ["gatsby"]
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      impliedStrict: true,
+      classes: true
     }
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "jest": true
+  env: {
+    browser: true,
+    node: true,
+    jest: true
   },
-  "rules": {
+  rules: {
     "no-debugger": 0,
     "no-alert": 0,
     "no-unused-vars": [
       1,
       {
-        "argsIgnorePattern": "res|next|^err"
+        argsIgnorePattern: "res|next|^err"
       }
     ],
     "prefer-const": [
       "error",
       {
-        "destructuring": "all",
+        destructuring: "all"
       }
     ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
+    "arrow-body-style": [2, "as-needed"],
     "no-unused-expressions": [
       2,
       {
-        "allowTaggedTemplates": true
+        allowTaggedTemplates: true
       }
     ],
     "no-param-reassign": [
       2,
       {
-        "props": false
+        props: false
       }
     ],
     "no-console": 0,
     "import/prefer-default-export": 0,
-    "import": 0,
+    import: 0,
     "func-names": 0,
     "import/no-extraneous-dependencies": 0,
     "space-before-function-paren": 0,
@@ -68,68 +64,57 @@ module.exports = {
     "react/destructuring-assignment": 0,
     "react/no-danger": 0,
     "react/no-unescaped-entities": 0,
-    "react/jsx-max-props-per-line": [2, {
-      "maximum": 1,
-      "when": "multiline",
-    }],
+    "react/jsx-max-props-per-line": [
+      2,
+      {
+        maximum: 1,
+        when: "multiline"
+      }
+    ],
     "jsx-a11y/accessible-emoji": 0,
     "react/jsx-filename-extension": [
       1,
       {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
+        extensions: [".js", ".jsx"]
       }
     ],
-    "radix": 0,
+    radix: 0,
     "no-shadow": [
       2,
       {
-        "hoist": "all",
-        "allow": [
-          "resolve",
-          "reject",
-          "done",
-          "next",
-          "err",
-          "error"
-        ]
+        hoist: "all",
+        allow: ["resolve", "reject", "done", "next", "err", "error"]
       }
     ],
-    "quotes": [
+    quotes: [
       2,
       "single",
       {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
+        avoidEscape: true,
+        allowTemplateLiterals: true
       }
     ],
     "prettier/prettier": [
       "error",
       {
-        "trailingComma": "all",
-        "singleQuote": true,
-        "printWidth": 100,
-        "semi": true,
-        "bracketSpacing": true,
-        "jsxBracketSameLine": false,
-        "arrowParens": "avoid",
-        "useTabs": false,
-        "tabWidth": 2
+        trailingComma: "all",
+        singleQuote: true,
+        printWidth: 100,
+        semi: true,
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: "avoid",
+        useTabs: false,
+        tabWidth: 2
       }
     ],
     "jsx-a11y/href-no-hash": "off",
     "jsx-a11y/anchor-is-valid": [
       "warn",
       {
-        "aspects": [
-          "invalidHref"
-        ]
+        aspects: ["invalidHref"]
       }
     ]
   },
-  "plugins": [
-    "prettier"
-  ]
-}
+  plugins: ["prettier"]
+};
