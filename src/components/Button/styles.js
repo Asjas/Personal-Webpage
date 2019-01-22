6f90474af2c6ece5e381825e85fd82cad4fd6584
Asjas <asjas@outlook.com>
2019-01-22 19:03:03 +0200

    Make the button smaller and inverted the hover color
    The button was too large so I made it a bit smaller.
    I also inverted the color of the button on hover and focus states

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,21 +4,19 @@ import { media } from '../../utils/media';
 export const Button = styled.button`
   background-color: ${props => props.theme.maroon};
   border-radius: 4px;
-  border: ${props => props.theme.maroon};
+  border: 2px solid ${props => props.theme.maroon};
   color: #fff;
   cursor: pointer;
   font-family: Contender;
   font-size: ${props => props.theme.fontXL};
   outline: 0;
-  padding: 7px;
   text-transform: capitalize;
   width: 100%;
 
-  &:hover {
-    outline: ${props => props.theme.blackBorderMd};
-  }
-
+  &:hover,
   &:focus {
-    outline: ${props => props.theme.blackBorderMd};
+    background-color: #fff;
+    color: ${props => props.theme.maroon};
+    border: 2px solid ${props => props.theme.maroon};
   }
 `;
