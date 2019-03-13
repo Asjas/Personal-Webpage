c5fe20bc7df5a6462536fc49214f9051ad5c6151
Asjas <asjas@outlook.com>
2019-03-13 22:03:21 +0200

    update ErrorBoundary component

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import hub from '../utils/sentry';
 
 class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
   componentDidCatch(error, errorInfo) {
     hub.withScope(scope => {
       Object.keys(errorInfo).forEach(key => {
@@ -13,7 +23,18 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
+    const { hasError } = this.state;
     const { children } = this.props;
+
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <p className="errorboundary-content">
+          Something went wrong displaying this section of the website. A log of the error
+          has been sent to the website owner.
+        </p>
+      );
+    }
 
     return children;
   }
