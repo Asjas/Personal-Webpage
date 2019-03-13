20666626a8cd8854a29d934d66cd367439f87b23
Asjas <asjas@outlook.com>
2019-03-13 22:03:10 +0200

    create and style newsletter signup

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState, memo } from 'react';
+import Button from './Button';
 
-function Newsletter() {
+function NewsletterSubscribed() {
   return (
     <section className="newsletter">
-      <p>Newsletter</p>
+      <h3 className="newsletter-heading">Success!</h3>
+      <p className="newsletter-content">You've been subscribed to my newsletter.</p>
     </section>
   );
+}
+
+const NewsletterForm = memo(function NewsletterForm(props) {
+  const { handleChange, handleSubmit } = props;
+
+  return (
+    <section className="newsletter">
+      <form className="newsletter-form" onSubmit={handleSubmit}>
+        <h3 className="newsletter-heading">Subscribe to my newsletter.</h3>
+        <p className="newsletter-content">
+          I send a newsletter about every 2 weeks that contains news updates surrounding
+          Web Development. I also include links to new blog posts that I write.
+        </p>
+        <label htmlFor="newsletter-input">
+          Enter your email:
+          <input
+            className="newsletter-input"
+            id="newsletter-input"
+            type="email"
+            onChange={handleChange}
+            placeholder="Enter your email"
+            required
+          />
+        </label>
+        <Button />
+      </form>
+    </section>
+  );
+});
+
+function Newsletter() {
+  const [email, setEmail] = useState('');
+  const [subscribed, setSubscribed] = useState(false);
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubscribed(true);
+  }
+
+  if (subscribed) {
+    return <NewsletterSubscribed />;
+  }
+  return <NewsletterForm handleChange={handleChange} handleSubmit={handleSubmit} />;
 }
 
 export default Newsletter;
