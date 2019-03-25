6364389727618482237c3427e5268cf29670f6dc
A-J Roos <asjas@outlook.com>
2019-03-25 14:22:39 +0200

    install missing testing packages and update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,24 @@
 const { defaults } = require('jest-config');
 
 module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/jest/jest.setup.js',
-  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, '__snapshots__', '<rootDir>/.cache'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    ...defaults.testPathIgnorePatterns,
+    '__snapshots__',
+    'node_modules',
+    '.cache',
+  ],
   transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
   transform: {
-    '.(js)': '<rootDir>/jest/jest-preprocess.js',
+    '.(js)': '<rootDir>/jest-preprocess.js',
   },
   globals: {
     __PATH_PREFIX__: '',
   },
-  testURL: 'http://localhost/',
-  setupFiles: ['<rootDir>/jest/loadershim.js'],
+  setupFiles: ['<rootDir>/loadershim.js'],
   moduleNameMapper: {
     '.+\\.(css|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '.+\\.(jpg|jpeg|png|otf|webp|svg|ttf|woff|woff2|mp3)$': '<rootDir>/__mocks__/fileMock.js',
   },
   testRegex: '\\.test\\.js',
   moduleFileExtensions: ['js', 'json'],
