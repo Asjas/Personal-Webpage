5add75ac4a7b838c61a99f238fa58e12566df4ed
Asjas <asjas@outlook.com>
2019-04-23 21:24:39 +0200

    update viewport meta tag

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ class HTML extends React.Component {
       <html lang="en" {...this.props.htmlAttributes}>
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
           <link rel="shortcut icon" href="https://storage.googleapis.com/asjas.co.za/favicon.ico" />
           <script
