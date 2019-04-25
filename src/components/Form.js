94ce562b99af35cccdbce423c2bb899f01d8c177
Asjas <asjas@outlook.com>
2019-04-25 22:01:56 +0200

    remove formik and add recaptcha to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,67 +1,51 @@
 import React from 'react';
-import { Formik } from 'formik';
+import Helmet from 'react-helmet';
 import StyledButton from './Button';
 
+function handleSubmit(event) {
+  event.stopDefault();
+}
+
 const ContactForm = () => (
-  <Formik
-    initialValues={{ name: '', email: '', message: '' }}
-    validate={values => {
-      const errors = {};
-
-      if (!values.name) {
-        errors.name = 'Required!';
-      }
-
-      if (!values.email) {
-        errors.email = 'Required!';
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-      }
-
-      if (!values.message) {
-        errors.message = 'Required!';
-      }
-
-      return errors;
-    }}
-    onSubmit={(values, { resetForm }) => {
-      setTimeout(() => {
-        resetForm();
-      }, 600);
-    }}
-  >
-    {({ isSubmitting }) => (
-      <form>
-        <fieldset
-          data-testid="form-disabled"
-          disabled={isSubmitting}
-          aria-busy={isSubmitting}
-        >
-          <label htmlFor="name">
-            <span>Enter your name:</span>
-            <span name="name" component="span" />
-            <input id="name" type="text" name="name" placeholder="full name" />
-          </label>
-          <label htmlFor="email">
-            <span>Enter your email:</span>
-            <span name="email" component="span" />
-            <input id="email" type="email" name="email" placeholder="example@gmail.com" />
-          </label>
-          <label htmlFor="message">
-            <span>Enter a message:</span>
-            <span name="message" component="span" />
-            <textarea
-              component="textarea"
-              id="message"
-              name="message"
-              placeholder="type your message here"
-            />
-          </label>
-          <StyledButton />
-        </fieldset>
-      </form>
-    )}
-  </Formik>
+  <>
+    <Helmet>
+      <script src="https://www.google.com/recaptcha/api.js?render=6LfRSKAUAAAAAOWRrjorE2kWWLN_aRlCuhTu6x-b" />
+    </Helmet>
+    <form name="contact" onSubmit={handleSubmit} data-netlify-recaptcha="true" data-netlify="true">
+      <fieldset data-testid="form-disabled">
+        <label htmlFor="name">
+          <span>Enter your name:</span>
+          <span name="name" component="span" />
+          <input id="name" type="text" name="name" placeholder="full name" />
+        </label>
+        <label htmlFor="email">
+          <span>Enter your email:</span>
+          <span name="email" component="span" />
+          <input id="email" type="email" name="email" placeholder="example@gmail.com" />
+        </label>
+        <label htmlFor="message">
+          <span>Enter a message:</span>
+          <span name="message" component="span" />
+          <textarea
+            component="textarea"
+            id="message"
+            name="message"
+            placeholder="type your message here"
+          />
+        </label>
+        {typeof window !== 'undefined' &&
+          window.grecaptcha &&
+          window.grecaptcha.ready(function() {
+            window.grecaptcha
+              .execute('6LfRSKAUAAAAAOWRrjorE2kWWLN_aRlCuhTu6x-b', { action: 'contactpage' })
+              .then(function(token) {
+                console.log(token);
+              });
+          })}
+        <StyledButton />
+      </fieldset>
+    </form>
+  </>
 );
 
 export default ContactForm;
