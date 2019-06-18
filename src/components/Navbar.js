09f9243638bc0d167a690e37d5312e106b020fba
A-J Roos <asjas@outlook.com>
2019-06-18 15:30:14 +0200

    Remove React-Spring animation from Hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
 import React, { useState, memo } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import { useTransition, animated } from 'react-spring';
 
 import Footer from './Footer';
 
-function HamburgerIcon({ handleClick, handleKeyDown, style }) {
+function HamburgerIcon({ handleClick, handleKeyDown }) {
   return (
-    <animated.a
+    <a
       className="hamburger__menu"
       onClick={handleClick}
       onKeyDown={handleKeyDown}
       tabIndex={0}
-      style={style}
       role="button"
       aria-labelledby="menu"
       aria-pressed="false"
@@ -23,18 +21,17 @@ function HamburgerIcon({ handleClick, handleKeyDown, style }) {
       <svg version="1.1" viewBox="0 0 32 32" width="32px" height="32px" xmlSpace="preserve">
         <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
       </svg>
-    </animated.a>
+    </a>
   );
 }
 
-function CloseIcon({ handleClick, handleKeyDown, style }) {
+function CloseIcon({ handleClick, handleKeyDown }) {
   return (
-    <animated.a
+    <a
       className="hamburger__close"
       onClick={handleClick}
       onKeyDown={handleKeyDown}
       tabIndex={0}
-      style={style}
       role="button"
       aria-labelledby="menu"
       aria-pressed="true"
@@ -47,17 +44,12 @@ function CloseIcon({ handleClick, handleKeyDown, style }) {
           <path d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z " />
         </g>
       </svg>
-    </animated.a>
+    </a>
   );
 }
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
-  const transitions = useTransition(isOpen, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  });
 
   function handleClick() {
     setIsOpen(!isOpen);
@@ -73,22 +65,10 @@ function Nav() {
 
   return (
     <>
-      {transitions.map(({ item, key, props }) =>
-        item ? (
-          <CloseIcon
-            key={key}
-            style={props}
-            handleClick={handleClick}
-            handleKeyDown={handleKeyDown}
-          />
-        ) : (
-          <HamburgerIcon
-            key={key}
-            style={props}
-            handleClick={handleClick}
-            handleKeyDown={handleKeyDown}
-          />
-        ),
+      {isOpen ? (
+        <CloseIcon handleClick={handleClick} handleKeyDown={handleKeyDown} />
+      ) : (
+        <HamburgerIcon handleClick={handleClick} handleKeyDown={handleKeyDown} />
       )}
 
       <div className={isOpen ? 'sidenav sidenav--show' : 'sidenav'}>
@@ -129,13 +109,11 @@ function Nav() {
 HamburgerIcon.propTypes = {
   handleClick: PropTypes.func.isRequired,
   handleKeyDown: PropTypes.func.isRequired,
-  style: PropTypes.object.isRequired,
 };
 
 CloseIcon.propTypes = {
   handleClick: PropTypes.func.isRequired,
   handleKeyDown: PropTypes.func.isRequired,
-  style: PropTypes.object.isRequired,
 };
 
 export default memo(Nav);
