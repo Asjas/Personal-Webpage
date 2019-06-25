ddc8ca4e144c38b94f2b6f2d22b75bc409cc3004
Asjas <asjas@outlook.com>
2019-06-26 00:04:08 +0200

    Install and configure Styled-Components

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   plugins: [
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-twitter',
