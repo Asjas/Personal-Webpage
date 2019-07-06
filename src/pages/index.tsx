0add6b4131437316f400f7118aecd104497d71df
Asjas <asjas@outlook.com>
2019-07-06 23:04:10 +0200

    Update React  usage on Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { memo } from 'react';
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 
@@ -9,11 +9,14 @@ interface Props {
   data: {
     file: {
       childImageSharp: {
-        fixed: {
-          width: number;
-          height: number;
+        fluid: {
+          aspectRatio: number;
+          base64: string;
+          sizes: string;
           src: string;
           srcSet: string;
+          srcSetWebp: string;
+          srcWebp: string;
         };
       };
     };
@@ -24,27 +27,31 @@ export const HOME_PAGE_IMAGE = graphql`
   query HOME_PAGE_IMAGE {
     file(relativePath: { regex: "/messy-desk.jpg/" }) {
       childImageSharp {
-        fixed(height: 400, quality: 95) {
-          ...GatsbyImageSharpFixed_withWebp
+        fluid(maxWidth: 700, quality: 95) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
   }
 `;
 
-const IndexPage: React.FunctionComponent<Props> = ({ data }): React.ReactElement => (
-  <>
-    <SEO />
-    <Layout>
-      <section className="homepage">
-        <p className="homepage__quote">
-          "There are only two hard things in Computer Science: cache invalidation and naming things"
-          - Phil Karlton
-        </p>
-        <Img className="homepage__image" fixed={data.file.childImageSharp.fixed} alt="" />
-      </section>
-    </Layout>
-  </>
+const IndexPage: React.FunctionComponent<Props> = memo(
+  ({ data }): React.ReactElement => (
+    <>
+      <SEO />
+      <Layout>
+        <section className="homepage">
+          <p className="homepage__quote">
+            "There are only two hard things in Computer Science: cache invalidation and naming
+            things" - Phil Karlton
+          </p>
+          <Img className="homepage__image" fluid={data.file.childImageSharp.fluid} alt="" />
+        </section>
+      </Layout>
+    </>
+  ),
 );
+
+IndexPage.displayName = 'IndexPage';
 
 export default IndexPage;
