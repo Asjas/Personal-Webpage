a6ce9014e798317d504d54bc777b1766fc98f2e9
Asjas <asjas@outlook.com>
2019-07-06 22:54:06 +0200

    Update React  usage on Project Page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { memo } from 'react';
 import Img from 'gatsby-image';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
@@ -35,37 +35,41 @@ interface Props {
   };
 }
 
-const Project: React.FunctionComponent<Props> = ({ data }): React.ReactElement => (
-  <section className="card">
-    <Img
-      className="card__image"
-      title={data.title.text}
-      alt={data.image.alt}
-      fluid={data.image.localFile.childImageSharp.fluid}
-    />
-    <h2 className="card__title">{data.title.text}</h2>
-    <p className="card__text">{data.description.text}</p>
-    <OutboundLink
-      className="card__button github"
-      href={data.github_url.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      tabIndex={0}
-      role="button"
-    >
-      <span className="button__content">Github</span>
-    </OutboundLink>
-    <OutboundLink
-      className="card__button website"
-      href={data.website_url.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      tabIndex={0}
-      role="button"
-    >
-      <span className="button__content">Website</span>
-    </OutboundLink>
-  </section>
+const Project: React.FunctionComponent<Props> = memo(
+  ({ data }): React.ReactElement => (
+    <section className="card">
+      <Img
+        className="card__image"
+        title={data.title.text}
+        alt={data.image.alt}
+        fluid={data.image.localFile.childImageSharp.fluid}
+      />
+      <h2 className="card__title">{data.title.text}</h2>
+      <p className="card__text">{data.description.text}</p>
+      <OutboundLink
+        className="card__button github"
+        href={data.github_url.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        tabIndex={0}
+        role="button"
+      >
+        <span className="button__content">Github</span>
+      </OutboundLink>
+      <OutboundLink
+        className="card__button website"
+        href={data.website_url.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        tabIndex={0}
+        role="button"
+      >
+        <span className="button__content">Website</span>
+      </OutboundLink>
+    </section>
+  ),
 );
+
+Project.displayName = 'Project';
 
 export default Project;
