e4c59c54298a5cbef2b6a4357dd57ac8c6684927
Asjas <asjas@outlook.com>
2019-07-06 22:53:08 +0200

    Update React  usage on BlogEntryCard

--- src/components/BlogEntryCard.tsx
+++ src/components/BlogEntryCard.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { memo } from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
 import { format, formatDistance } from 'date-fns';
@@ -40,53 +40,57 @@ const calc = (x: number, y: number): number[] => [
 const trans = (x: number, y: number, s: number): string =>
   `perspective(1200px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-const BlogEntryCard: React.FunctionComponent<Props> = ({ node }): React.ReactElement => {
-  const [{ xys }, set] = useSpring(() => ({
-    xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 },
-  }));
+const BlogEntryCard: React.FunctionComponent<Props> = memo(
+  ({ node }): React.ReactElement => {
+    const [{ xys }, set] = useSpring(() => ({
+      xys: [0, 0, 1],
+      config: { mass: 5, tension: 350, friction: 40 },
+    }));
 
-  return (
-    <animated.section
-      className="blogpost__section"
-      key={node.id}
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      style={{ transform: xys.interpolate(trans) }}
-    >
-      <Img
-        className="blogpost__image"
-        fluid={node.frontmatter.featured_image.childImageSharp.fluid}
-        alt=""
-      />
-      <Link className="blogpost__link" to={`/blog${node.fields.slug}`}>
-        <h2 className="blogpost__heading">{node.frontmatter.title}</h2>
-      </Link>
-      <span className="blogpost__date">
-        Published:{' '}
-        <time dateTime={format(new Date(node.frontmatter.date), 'yyyy-MM-dd')}>
-          {formatDistance(new Date(node.frontmatter.date), new Date(), {
-            addSuffix: true,
-          })}
-        </time>
-      </span>
-      <span className="blogpost__date">
-        Last Updated:{' '}
-        <time dateTime={format(new Date(node.frontmatter.updated_at), 'yyyy-MM-dd')}>
-          {formatDistance(new Date(node.frontmatter.updated_at), new Date(), {
-            addSuffix: true,
-          })}
-        </time>
-      </span>
-      <ul className="blogpost__tags">
-        {node.frontmatter.tags.map(tag => (
-          <Link key={tag} className="blogpost__tag" to={`/tags/${tag}`}>
-            {`#${tag}`}
-          </Link>
-        ))}
-      </ul>
-    </animated.section>
-  );
-};
+    return (
+      <animated.section
+        className="blogpost__section"
+        key={node.id}
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: xys.interpolate(trans) }}
+      >
+        <Img
+          className="blogpost__image"
+          fluid={node.frontmatter.featured_image.childImageSharp.fluid}
+          alt=""
+        />
+        <Link className="blogpost__link" to={`/blog${node.fields.slug}`}>
+          <h2 className="blogpost__heading">{node.frontmatter.title}</h2>
+        </Link>
+        <span className="blogpost__date">
+          Published:{' '}
+          <time dateTime={format(new Date(node.frontmatter.date), 'yyyy-MM-dd')}>
+            {formatDistance(new Date(node.frontmatter.date), new Date(), {
+              addSuffix: true,
+            })}
+          </time>
+        </span>
+        <span className="blogpost__date">
+          Last Updated:{' '}
+          <time dateTime={format(new Date(node.frontmatter.updated_at), 'yyyy-MM-dd')}>
+            {formatDistance(new Date(node.frontmatter.updated_at), new Date(), {
+              addSuffix: true,
+            })}
+          </time>
+        </span>
+        <ul className="blogpost__tags">
+          {node.frontmatter.tags.map(tag => (
+            <Link key={tag} className="blogpost__tag" to={`/tags/${tag}`}>
+              {`#${tag}`}
+            </Link>
+          ))}
+        </ul>
+      </animated.section>
+    );
+  },
+);
+
+BlogEntryCard.displayName = 'BlogEntryCard';
 
 export default BlogEntryCard;
