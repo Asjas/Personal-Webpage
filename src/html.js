31543fdcb9da407c31ebe879a43db544847f4a4c
Asjas <asjas@outlook.com>
2019-07-19 23:41:47 +0200

    replace modern-normalize npm package with CDN link

--- src/html.js
+++ src/html.js
@@ -5,7 +5,16 @@ class HTML extends React.Component {
   render() {
     return (
       <html lang="en" {...this.props.htmlAttributes}>
-        <head>{this.props.headComponents}</head>
+        <head>
+          {this.props.headComponents}
+          {/* Normalize */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/0.5.0/modern-normalize.min.css"
+            integrity="sha256-N6+kUxTWxpqVK+BrPWt3t4jeOWPtp37RZEbm5n9X+8U="
+            crossOrigin="anonymous"
+          />
+        </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
           <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
