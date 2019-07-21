1a7672f728d65b7618598c78935fa708f0df5821
Asjas <asjas@outlook.com>
2019-07-21 14:43:55 +0200

    add firebase and firebase performance tracking

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,30 @@
 import React, { ReactNode } from 'react';
-import { ThemeProvider } from '../../utils/themed-styled-components';
-import { GlobalStyle, myTheme } from '../../utils/theme';
+import * as firebase from 'firebase/app';
+import 'firebase/performance';
 
 import ErrorBoundary from '../ErrorBoundary';
 import Navbar from '../Navbar';
 
 import setupLogRocket from '../../utils/logrocket';
+import { ThemeProvider } from '../../utils/themed-styled-components';
+import { GlobalStyle, myTheme } from '../../utils/theme';
 
 import * as Styled from './style';
 
 setupLogRocket();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyARArNM5Ps9wG-zSOS8Fe-tIzmNIu2lGBw',
+  authDomain: 'personal-website-1213.firebaseapp.com',
+  databaseURL: 'https://personal-website-1213.firebaseio.com',
+  projectId: 'personal-website-1213',
+  storageBucket: 'personal-website-1213.appspot.com',
+  messagingSenderId: '436200487184',
+  appId: '1:436200487184:web:a7cc904f69b93dd6',
+};
+
+firebase.initializeApp(firebaseConfig);
+const perf = firebase.performance();
 
 interface Props {
   children: ReactNode;
