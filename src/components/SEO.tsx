f81e9aec09d0fa3346d7e86730bf001f62a7f04e
Asjas <asjas@outlook.com>
2019-07-21 14:44:14 +0200

    Update React-Helmet to 6.0.0@beta

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 const GET_SEO_METADATA = graphql`
