6701234225eaedfe7ecb2cae07ba7fc688efdeef
Asjas <asjas@outlook.com>
2019-09-13 15:56:00 +0200

    Aggregate HTML Meta tags in the SEO components
    * Move Google Fonts, Favicon Icons and Normalize.css tags to the
    SEO components.
    * Add DNS Prefetch and Preconnect tags for most of the 3rd party
    services

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -106,17 +106,32 @@ const SEO: React.FunctionComponent<Props> = ({
 
   return (
     <Helmet>
-      {/* General Tags */}
+      {/* General HTML Tags */}
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <link rel="preconnect" href="https://storage.googleapis.com" crossOrigin="true" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
-
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <link rel="canonical" href={seo.siteUrl} />
       <meta name="google-site-verification" content={seo.googleSiteVerification} />
+
+      {/* DNS Prefetching */}
+      <link rel="dns-prefetch" href="//fonts.googleapis.com"></link>
+      <link rel="dns-prefetch" href="//fonts.gstatic.com"></link>
+      <link rel="dns-prefetch" href="//www.gstatic.com/"></link>
+      <link rel="dns-prefetch" href="//cdnjs.cloudflare.com"></link>
+      <link rel="dns-prefetch" href="//cdn.sanity.io"></link>
+      <link rel="dns-prefetch" href="//cdn.lr-ingest.io"></link>
+      <link rel="dns-prefetch" href="//cdn.polyfill.io"></link>
+
+      {/* DNS Pre-Connection */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link rel="preconnect" href="https://www.gstatic.com/" crossOrigin="true" />
+      <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="true" />
+      <link rel="preconnect" href="https://cdn.sanity.io" crossOrigin="true" />
+      <link rel="preconnect" href="https://cdn.lr-ingest.io" crossOrigin="true" />
+      <link rel="preconnect" href="https://cdn.polyfill.io" crossOrigin="true" />
 
       {/* Schema.org tags */}
       <script type="application/ld+json">{JSON.stringify(schemaOrgJSONLD)}</script>
@@ -126,11 +141,7 @@ const SEO: React.FunctionComponent<Props> = ({
       <meta property="og:url" content={seo.siteUrl} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
-      {isBlogPost ? (
-        <meta property="og:type" content="article" />
-      ) : (
-        <meta property="og:type" content="website" />
-      )}
+      {isBlogPost ? <meta property="og:type" content="article" /> : <meta property="og:type" content="website" />}
 
       {/* Twitter Cards */}
       <meta name="twitter:card" content="summary" />
@@ -139,15 +150,31 @@ const SEO: React.FunctionComponent<Props> = ({
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
 
-      {/* Fonts */}
+      {/* Normalize.css */}
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/0.5.0/modern-normalize.min.css"
+        integrity="sha256-N6+kUxTWxpqVK+BrPWt3t4jeOWPtp37RZEbm5n9X+8U="
+        crossOrigin="anonymous"
+      />
+
+      {/* Google Fonts */}
       <link
         href="https://fonts.googleapis.com/css?family=Indie+Flower|Luckiest+Guy|Playfair+Display:400,500&text=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`'!:;_-=,.?|0123456789&display=swap"
         rel="stylesheet"
       />
-      <link
-        href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i&display=swap"
-        rel="stylesheet"
-      />
+      <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i&display=swap" rel="stylesheet" />
+
+      {/* Favicon Icons */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/assets/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/assets/favicons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   );
 };
--- src/html.js
+++ src/html.js
@@ -5,41 +5,7 @@ class HTML extends React.Component {
   render() {
     return (
       <html lang="en" {...this.props.htmlAttributes}>
-        <head>
-          {this.props.headComponents}
-          {/* Normalize */}
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/0.5.0/modern-normalize.min.css"
-            integrity="sha256-N6+kUxTWxpqVK+BrPWt3t4jeOWPtp37RZEbm5n9X+8U="
-            crossOrigin="anonymous"
-          />
-
-          {/* Favicon Icons */}
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/assets/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/assets/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/assets/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/assets/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-          <link rel="shortcut icon" href="/assets/favicons/favicon.ico" />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="msapplication-config" content="/assets/favicons/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
-        </head>
+        <head>{this.props.headComponents}</head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
           <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
