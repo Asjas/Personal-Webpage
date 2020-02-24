01ce02a3e99e33c4fdd722c704348751a0cbae82
A-J Roos <asjas@outlook.com>
2020-02-24 20:54:37 +0200

    Add HTTP Security Headers to Cloudflare Worker

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,13 +1,17 @@
 import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler';
 import { log } from './sentry';
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
+// HTTP Security Headers
+const addHeaders = {
+  'Expect-CT': 'max-age=604800, report-uri https://asjas.report-uri.com/r/d/ct/enforce; report-to default',
+  'Referrer-Policy': 'same-origin',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+  'X-Content-Type-Options': 'nosniff',
+  'X-Frame-Options': 'DENY',
+  'X-XSS-Protection': '1; mode=block',
+  Server: 'Snoopy',
+};
+
 const DEBUG = false;
 
 addEventListener('fetch', event => {
@@ -28,25 +32,24 @@ addEventListener('fetch', event => {
 });
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url);
   let options = {};
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
 
   try {
     if (DEBUG) {
-      // customize caching
       options.cacheControl = {
         bypassCache: true,
       };
     }
-    return await getAssetFromKV(event, options);
+    const asset = await getAssetFromKV(event, options);
+
+    let response = new Response(asset.body, asset);
+
+    Object.keys(addHeaders).forEach(function(header) {
+      response.headers.set(header, addHeaders[header]);
+    });
+
+    return response;
   } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
     if (!DEBUG) {
       try {
         let notFoundResponse = await getAssetFromKV(event, {
@@ -60,25 +63,4 @@ async function handleEvent(event) {
     log(e, event.request);
     return new Response(e.message || e.toString(), { status: 500 });
   }
-}
-
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request);
-    let url = new URL(defaultAssetKey.url);
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/');
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey);
-  };
 }
