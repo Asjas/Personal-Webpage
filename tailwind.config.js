2ea44b9b1ae6ae91e119a45ef30cecc91d2b3fdc
A-J Roos <asjas@outlook.com>
2020-06-19 21:59:04 +0200

    setup tailwindcss for styles

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.js"],
+  theme: {
+    fontFamily: {
+      display: ["Playfair Display", "serif"],
+      body: ["Lato", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#212121",
+        secondary: "#fcfcfc",
+      },
+    },
+  },
+};
